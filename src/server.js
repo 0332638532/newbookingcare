e269250a9d11687ccfd5cfaaf89742fc2e15b4b6
Khanh Pham <khanhphamvan1910@gmail.com>
2022-10-12 09:46:04 +0700

    connect and GET data

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initwebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 
 require("dotenv").config();
 let app = express();
 
 viewEngine(app);
 initwebRoutes(app);
+connectDB();
 
 app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: true }));
